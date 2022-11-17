51d6af5dd313ce9161d44d3ca0ba9b61cfed6151
JailedBird <zjc986812982@gmail.com>
2022-11-17 11:52:56 +0800

    feat: complete jitpack release demo

--- app/src/main/java/com/jailedbird/jitpack/MainActivity.kt
+++ app/src/main/java/com/jailedbird/jitpack/MainActivity.kt
@@ -3,7 +3,7 @@ package com.jailedbird.jitpack
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-//import com.jailedbird.jitpack.lib1.Lib1
+import com.jailedbird.jitpack.lib1.Lib1
 
 class MainActivity : AppCompatActivity() {
     companion object{
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Log.d(TAG, "onCreate: ${Lib1.info}")
+        Log.d(TAG, "onCreate: ${Lib1.info}")
 
     }
 }
